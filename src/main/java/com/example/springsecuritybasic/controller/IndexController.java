51b146ca2778a373c84a023976dc041ea4a09028
임빈영 <qlsdud960604@gmail.com>
2021-09-20 16:10:44 +0900

    시큐리티 설정

--- src/main/java/com/example/springsecuritybasic/controller/IndexController.java
+++ src/main/java/com/example/springsecuritybasic/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.springsecuritybasic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller   // View를 리턴하겠다는 애너테이션
 public class IndexController {
@@ -9,5 +10,41 @@ public class IndexController {
     @GetMapping({"", "/"})
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    @ResponseBody
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    @ResponseBody
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    @ResponseBody
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    @ResponseBody
+    public String joinProc() {
+        return "회원가입이 완료되었습니다.";
     }
 }
